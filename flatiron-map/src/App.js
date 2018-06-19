d1e9ed9bd71febfc0d88ffbf6d6f9dde1bdad76f
Daniel Brazel <dbrazel4@gmail.com>
2018-06-19 13:18:02 -0400

    Form written

--- flatiron-map/src/App.js
+++ flatiron-map/src/App.js
@@ -12,8 +12,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
-      <FIMap />
+        <ReservationForm />
+        <FIMap />
       </div>
     );
   }
--- flatiron-map/src/components/reservation_form.js
+++ flatiron-map/src/components/reservation_form.js
@@ -1,5 +1,26 @@
 import React, { Component } from 'react';
 
 export default class ReservationForm extends Component {
-
+  render(){
+    return(
+      <form>
+        <label for="event-title">Event Title</label>
+        <input type='text' id='event-title'/>
+        <label for="time">Time</label>
+        <input type='time' id='time'/>
+        <label for="room">Room</label>
+        <select id='room'>
+          <option value="phone-booth-1">Phone Booth 1</option>
+          <option value="phone-booth-2">Phone Booth 2</option>
+          <option value="phone-booth-3">Phone Booth 3</option>
+          <option value="phone-booth-4">Phone Booth 4</option>
+          <option value="phone-booth-5">Phone Booth 5</option>
+          <option value="phone-booth-6">Phone Booth6</option>
+          <option value ="turing">Turing</option>
+          <option value ="kay">Kay</option>
+          <option value ="babbage">Babbage</option>
+        </select>
+      </form>
+    )
+  }
 }

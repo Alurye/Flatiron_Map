069c71c300523ef82610583e141770099e78fd53
Daniel Brazel <dbrazel4@gmail.com>
2018-06-20 11:41:23 -0400

    fixed fetch

--- flatiron-map/src/components/reservation_form.js
+++ flatiron-map/src/components/reservation_form.js
@@ -7,7 +7,8 @@ export default class ReservationForm extends Component {
     this.state = {
       eventName: "",
       time: "",
-      room: {}
+      room: "",
+      room_id: ""
     }
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -15,15 +16,16 @@ export default class ReservationForm extends Component {
     this.handleRoomChange = this.handleRoomChange.bind(this);
   }
 
-  componentWillReceiveProps(nextProps){
-    this.setState({
-      room: nextProps.rooms[0]
-    })
-  }
+  // componentWillReceiveProps(nextProps){
+  //   this.setState({
+  //     room: nextProps.rooms[0]
+  //   })
+  // }
 
   handleSubmit(event){
+
     event.preventDefault()
-    console.log('title', this.state.eventName, "time", this.state.time, "room_id", this.state.room.id, "room_name", this.state.room.name)
+    console.log('title', this.state.eventName, "time", this.state.time, "room_name", this.state.room)
     fetch('http://localhost:3000/api/v1/events', {
       method: 'POST',
       headers: {
@@ -33,11 +35,11 @@ export default class ReservationForm extends Component {
       body: JSON.stringify({
         title: this.state.eventName,
         time: this.state.time,
-        room_name: this.state.room.name,
-        room_id: this.state.room.id
+        room_name: this.state.room,
+        room_id: this.state.room_id
       })
     })
-    .then(res => res.json)
+    .then(res => res.json())
     .then(json => console.log(json))
   }
   handleNameChange(event){
@@ -52,27 +54,38 @@ export default class ReservationForm extends Component {
     })
   }
 
+  // handleFormInput = (e) => {
+  //   this.setState({
+  //     [e.target.name]: e.target.value
+  //   });
+  // }
+
   handleRoomChange(event){
+    event.preventDefault();
+    const idNumber = this.props.rooms.find(aRoom => aRoom.name === event.target.value);
+
     this.setState({
-      room: event.target.value
-    }, console.log("room is", this.state.room))
+      room: event.target.value,
+      room_id: idNumber.id
+    })
   }
 
 
 
   render(){
     // console.log("after props received room is:", this.state.room)
+    console.log(this.state);
     return(
       <form onSubmit={this.handleSubmit}>
         <label>Title
-        <input onChange={this.handleNameChange} value={this.state.eventName} type='text' id='event-title'/>
+        <input onChange={this.handleNameChange} name="title" value={this.state.eventName} type='text' id='event-title'/>
         </label><br/>
         <label>Time
-        <input type='datetime-local' value={this.state.time} onChange={this.handleTimeChange} id='time'/>
+        <input type='datetime-local' name="datetime" value={this.state.time} onChange={this.handleTimeChange} id='time'/>
         </label><br/>
         <label>Room
         <select id='room' value={this.state.room} onChange={this.handleRoomChange}>
-          {this.props.rooms.map(room =>{return <option value={room} key={room.id}>{room.name}</option>})}
+          {this.props.rooms.map(room =>{return <option value={room.name} id={room.id} key={room.id}>{room.name}</option>})}
         </select>
       </label><br/>
         <input type='submit' />

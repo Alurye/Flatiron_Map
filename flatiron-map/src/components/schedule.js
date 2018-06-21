82cd98de49683e94989bb1a36d8d39f07c51a6ac
Daniel Brazel <dbrazel4@gmail.com>
2018-06-21 15:29:22 -0400

    Made schedule pretty

--- flatiron-map/src/components/schedule.js
+++ flatiron-map/src/components/schedule.js
@@ -6,16 +6,22 @@ export default class Schedule extends Component {
     console.log("schedule level", this.props.events)
     return(
       <div>
-        {this.props.events.length === 0 ? null : <Table celled><Table.Row><Table.HeaderCell></Table.HeaderCell><Table.HeaderCell>{this.props.events[0].room_name}</Table.HeaderCell><Table.HeaderCell></Table.HeaderCell></Table.Row><Table.Row>
+        {this.props.events.length === 0 ? null : <div>
+          <h2>{this.props.events[0].room_name}</h2>
+          <Table celled>
+            <Table.Header>
+            <Table.Row>
           <Table.HeaderCell>Event Name</Table.HeaderCell>
           <Table.HeaderCell>Event Time</Table.HeaderCell>
-        </Table.Row>{this.props.events.map(ev => {
+        </Table.Row>
+        </Table.Header>
+        {this.props.events.map(ev => {
           let datetime = new Date(ev.time).toLocaleString()
           return <Table.Row>
             <Table.Cell>{ev.title}</Table.Cell>
             <Table.Cell>{datetime}</Table.Cell>
           </Table.Row>
-        })}</Table>}
+        })}</Table></div>}
       </div>
     )
   }
